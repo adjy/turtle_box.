61c82a9be540ff4acfa2f5829d36f970d5efb830
adjy <43525621+adjy@users.noreply.github.com>
2020-08-31 23:31:00 -0400

    ccode1.py

---
+++ code.py
@@ -0,0 +1,57 @@
+from turtle import*
+Screen()
+t=Turtle()
+t.color('red')
+t.shape('turtle')
+
+t.forward(100)
+t.right(90)
+t.backward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+t.penup()
+t.goto(50,50)
+
+t.pendown()
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+
+t.penup()
+t.left(90)
+t.forward(100)
+
+t.pendown()
+t.goto(0, 100)
+
+t.penup()
+t.goto(50,50)
+
+t.pendown()
+t.goto(100, 100)
+
+t.penup()
+t.goto(100, 0)
+
+t.pendown()
+t.goto(50, -50)
+
+t.penup()
+t.goto(-50, -50)
+
+t.pendown()
+t.goto(0, 0)
+
+t.penup()
+t.goto(200, 200)
+
+
